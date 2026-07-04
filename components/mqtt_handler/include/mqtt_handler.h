416ae407353789ef4b5af3c7ea7c7365cddf5e76
José Moraes <joseaugustomoraes@protonmail.com>
2026-07-04 17:58:25 -0300

    Adiciona pasta para o componente mqtt

---
+++ components/mqtt_handler/include/mqtt_handler.h
@@ -0,0 +1,6 @@
+#ifndef MQTT_H
+#define MQTT_H
+
+const int TESTE = 42;
+
+#endif
--- main/main.c
+++ main/main.c
@@ -1,6 +1,11 @@
+#include "esp_log.h"
+#include "mqtt_handler.h"
 #include <stdio.h>
 
-void app_main(void)
-{
+static const char* TAG = "estacao_esp";
 
+void app_main(void) {
+  ESP_LOGI(TAG, "%d\n", TESTE);
+
+  return;
 }
